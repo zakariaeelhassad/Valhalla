3ff52e428f8a77652d5575cb37de8ffd99955ede
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-16 11:38:39 +0000

    implémenter la logique de sélection et sauvegarde de l'équipe

---
+++ angular-frontend/src/app/pages/team-selection/team-selection.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-selection',
+  imports: [],
+  templateUrl: './team-selection.html',
+  styles: ``,
+})
+export class TeamSelection {}
