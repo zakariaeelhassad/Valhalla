8c71bd216e606f695ec076152ca39c32d1b41e57
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:10:34 +0100

    Mise à jour du repository UserTeamGameweekPoints

--- backend/src/main/java/com/example/backend/repository/UserTeamGameweekPointsRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserTeamGameweekPointsRepository.java
@@ -1,6 +1,6 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.UserTeamGameweekPoints;
+import com.example.backend.model.entity.UserTeamGameweekPoints;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
