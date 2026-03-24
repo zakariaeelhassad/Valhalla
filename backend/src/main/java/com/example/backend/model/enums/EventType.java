717f556d13ec1fafbdc38724f12757ec00b480e1
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:10:59 +0100

    Ajout des énumérations

---
+++ backend/src/main/java/com/example/backend/model/enums/EventType.java
@@ -0,0 +1,9 @@
+package com.example.backend.model.enums;
+
+public enum EventType {
+    GOAL,
+    ASSIST,
+    YELLOW_CARD,
+    RED_CARD,
+    APPEARANCE
+}
---
+++ backend/src/main/java/com/example/backend/model/enums/Position.java
@@ -0,0 +1,8 @@
+package com.example.backend.model.enums;
+
+public enum Position {
+    GK, // Goalkeeper
+    DEF, // Defender
+    MID, // Midfielder
+    FWD // Forward
+}
