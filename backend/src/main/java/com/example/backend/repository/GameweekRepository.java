7dec52d6f408c21921ab75f8ecb5661bbad3bb18
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:10:32 +0100

    Mise à jour du repository Gameweek

--- backend/src/main/java/com/example/backend/repository/GameweekRepository.java
+++ backend/src/main/java/com/example/backend/repository/GameweekRepository.java
@@ -1,6 +1,6 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.Gameweek;
+import com.example.backend.model.entity.Gameweek;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
