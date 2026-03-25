5de1114ed04cedf65000c29e67cf91e93a7077f0
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-25 15:08:06 +0100

    Suppression de l'ancienne logique monolithique de sélection d'équipe

--- angular-frontend/src/app/pages/team-selection/team-selection.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-team-selection',
-  imports: [],
-  templateUrl: './team-selection.html',
-  styles: ``,
-})
-export class TeamSelection {}
